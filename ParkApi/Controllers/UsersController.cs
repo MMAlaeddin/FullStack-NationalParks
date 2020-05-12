20189fdfe6c4723a814a990b1872e8f133aa2d4f
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-05-12 11:35:22 -0700

    figure out db
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-Authored-by: Dominique Youmans <ddyoumans@icloud.com>
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>

--- ParkApi/Controllers/UsersController.cs
+++ ParkApi/Controllers/UsersController.cs
@@ -1,47 +1,47 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
-using NationalParks.Services;
-using NationalParks.Models;
-using System.Linq;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.AspNetCore.Authorization;
+// // using NationalParks.Services;
+// using NationalParks.Models;
+// using System.Linq;
 
-namespace NationalParks.Controllers
-{
-  [Authorize]
-    [ApiController]
-    [Route("[controller]")]
+// namespace NationalParks.Controllers
+// {
+//   [Authorize]
+//     [ApiController]
+//     [Route("[controller]")]
 
-    public class UsersController : ControllerBase
-    {
-      private IUserService _userService;
+//     public class UsersController : ControllerBase
+//     {
+//       // private IUserService _userService;
 
-      public UsersController(IUserService userService)
-      {
-        _userService = userService;
-      }
+//       // public UsersController(IUserService userService)
+//       // {
+//       //   _userService = userService;
+//       // }
 
-      // POST /users/authenticate
-      [AllowAnonymous]
-      [HttpPost("authenticate")]
-      public IActionResult Authenticate([FromBody] User userParam)
-      {
-        var user = _userService.Authenticate(userParam.Email, userParam.Password);
+//       // POST /users/authenticate
+//       [AllowAnonymous]
+//       [HttpPost("authenticate")]
+//       public IActionResult Authenticate([FromBody] User userParam)
+//       {
+//         var user = _userService.Authenticate(userParam.Email, userParam.Password);
 
-        if (user == null)
-        {
-          return BadRequest(new { message = "Email or password is incorrect" });
-        }
-        else
-        {
-          return Ok(user);
-        }
-      }
+//         if (user == null)
+//         {
+//           return BadRequest(new { message = "Email or password is incorrect" });
+//         }
+//         else
+//         {
+//           return Ok(user);
+//         }
+//       }
       
-      // GET /users
-      [HttpGet]
-      public IActionResult GetAll()
-      {
-        var users = _userService.GetAll();
-        return Ok(users);
-      }
-    }
-}
+//       // GET /users
+//       [HttpGet]
+//       public IActionResult GetAll()
+//       {
+//         var users = _userService.GetAll();
+//         return Ok(users);
+//       }
+//     }
+// }
---
+++ ParkApi/Migrations/20200512182857_Initial.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace parksapifullstack.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Parks",
+                columns: table => new
+                {
+                    ParkId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    City = table.Column<string>(nullable: false),
+                    State = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(maxLength: 2000, nullable: true),
+                    ClimbingRoutes = table.Column<int>(nullable: false),
+                    Campgrounds = table.Column<int>(nullable: false),
+                    GeneralStores = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parks", x => x.ParkId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Campgrounds", "City", "ClimbingRoutes", "Description", "GeneralStores", "Name", "State" },
+                values: new object[,]
+                {
+                    { 2, 3, "Three Rivers", 1, "It's ancient", 4, "Sequoia National Park", "CA" },
+                    { 3, 9, "Yosemite National Park", 1348, "I met my friend Sam here", 8, "Yosemite National Park", "CA" },
+                    { 4, 4, "Grand Canyon", 46, "It big", 3, "Grand Canyon National Park", "AZ" },
+                    { 5, 17, "Glacier", 5, "I didn't get to see it 'cause Donald shut down the government when I got there, whatta loon.", 1, "Glacier National Park", "MT" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parks");
+        }
+    }
+}
